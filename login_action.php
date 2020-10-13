98704e31fbea4e68861aa4d5a5a081b9a5054970
Blake-Samani <Blake1.Samani@gmail.com>
2020-10-13 01:58:08 -0500

    added more files we need

--- login_action.php
+++ login_action.php
@@ -39,17 +39,31 @@ if($values = oci_fetch_array ($cursor)){
   $result = $result_array["flag"];
   $cursor = $result_array["cursor"];
 
+
+
+
   if ($result == false){
     display_oracle_error_message($cursor);
     die("Failed to create a new session");
   }
-  else {
-    // insert OK - we have created a new session
-    header("Location:welcomepage.php?sessionid=$sessionid");
-  }
+
+  //ORIGINAL CODE
+  // else {
+  //   // insert OK - we have created a new session
+  //   header("Location:welcomepage.php?sessionid=$sessionid");
+  // }
 }
 else { 
   // client username not found
   die ('Login failed.  Click <A href="login.html">here</A> to go back to the login page.');
-} 
+}
+
+// INSERT EXIST QUERY FOR CHECKING ADMIN PRIVLEGES
+// Logic
+// if the userid exists within the admind table, push to admin page
+// if the userid exists within student admin table, push to student admin page
+// if the userid exists within the sudent user table, push to student user page
+
+$sql = "select "
+
 ?>
